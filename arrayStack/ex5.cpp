85b4dc723ff964e1a48e5ba849605f7352d78592
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-19 20:50:30 -0300

    add ex5

---
+++ arrayStack/ex5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <string>
+
+void process_lines(const std::string& filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo: " << filename << std::endl;
+        return;
+    }
+    std::unordered_set<std::string> seen_once;
+    std::unordered_set<std::string> printed;
+    std::string line;
+
+    while (std::getline(infile, line)) {
+        if (seen_once.emplace(line).second)
+            continue;
+        if (printed.emplace(line).second)
+            std::cout << line << std::endl;
+    }
+
+    infile.close();
+}
+
+int main(int argc, char* argv[]){
+    if (argc != 2) {
+        std::cerr << "Uso: " << argv[0] << " <arquivo>" << std::endl;
+        return 1;
+    }
+    process_lines(argv[1]);
+    return 0;
+}
