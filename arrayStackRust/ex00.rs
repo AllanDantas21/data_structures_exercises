9e00425b69c9179fe1d484896093a3e2712f4f0d
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-18 23:18:27 -0300

    finish first ex

--- arrayStackRust/ex00.rs
+++ arrayStackRust/ex00.rs
@@ -3,19 +3,48 @@ use std::fs::File;
 use std::path::Path;
 use std::io::{self, BufRead};
 
+struct Stack<T> {
+    stack: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { stack: Vec::new() }
+    }
+
+    fn length(&self) -> usize {
+        self.stack.len()
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
+
+    fn push(&mut self, item: T) {
+        self.stack.push(item)
+    }
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("err");
+        eprintln!("Usage: {} <file_path>", args[0]);
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid number of arguments"));
     }
+
+    let mut s = Stack::new();
 
     let path = Path::new(&args[1]);
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
     for line in reader.lines() {
-        println!("{}", line?);
+        let line = line?;
+        s.push(line);
+    }
+    for _i in 0..s.length() {
+        println!("{}", s.pop().unwrap());
     }
     Ok(())
 }
